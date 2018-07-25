20821dd9c08ecac35d39c368a0fef867b5bc17e9
Derrick Stolee <dstolee@microsoft.com>
2018-07-25 12:56:22 -0400

    vdw: make svg images square
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- vdw/vdw-svg.py
+++ vdw/vdw-svg.py
@@ -22,16 +22,35 @@ colors = [ "#008000", "#800000", "#ffd42a", "#0000AA", "#9955FF" ];
 
 max_color = max(coloring);
 
-height=50;
-width=10;
+height=20;
+width=20;
 
-print "<svg height=\"%d\" width=\"%d\"" % (height, width * len(coloring));
+rows = 1;
+columns = len(coloring);
+
+while rows < columns:
+	rows += 1;
+	columns = (len(coloring) / rows) + 1;
+
+rows -= 1;
+columns = (len(coloring) / rows) + 1;
+
+print "<svg height=\"%d\" width=\"%d\"" % (height * rows, width * columns);
 print "   xmlns:svg=\"http://www.w3.org/2000/svg\"";
 print "   xmlns=\"http://www.w3.org/2000/svg\">";
 
+x = 0;
+y = 0;
+
 for i in range(len(coloring)):
-	print "<rect y=\"0\" x=\"%d\" width=\"%d\" height=\"%d\"" % (width * i, width, height);
+	if x >= columns:
+		x = 0;
+		y += 1;
+
+	print "<rect y=\"%d\" x=\"%d\" width=\"%d\" height=\"%d\"" % (height * y, width * x, width, height);
 	print "      style=\"fill:%s;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-opacity:1\" />" % colors[max_color - coloring[i]]; 
+	
+	x += 1;
 
 print "</svg>";
 
