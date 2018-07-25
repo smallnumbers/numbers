5fa1d143333116d7a120f82a027fb1082c44ae70
Derrick Stolee <dstolee@microsoft.com>
2018-07-25 08:40:30 -0400

    disc: slightly improve SAT formulation
    We can assert that the other initial conditions are false, which
    may help the prover.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- disc/disc.py
+++ disc/disc.py
@@ -4,8 +4,8 @@ import sys;
 import os;
 
 if len(sys.argv) < 2:
-    print "usage: disc.py <C> <N>"
-    exit(1);
+	print "usage: disc.py <C> <N>"
+	exit(1);
 
 from z3 import *;
 
@@ -16,30 +16,33 @@ variables = "";
 constraints = "";
 
 for i in range(1, N + 1):
-    variables += "(declare-const xp%d Bool)\n" % i;
-    variables += "(declare-const xm%d Bool)\n" % i;
-    constraints += "(assert (xor xp%d xm%d))\n" % (i,i);
+	variables += "(declare-const xp%d Bool)\n" % i;
+	variables += "(declare-const xm%d Bool)\n" % i;
+	constraints += "(assert (xor xp%d xm%d))\n" % (i,i);
 
 for d in range(1, N / C + 1):
-    i = d;
-    for t in range(0, 2 * C + 1):
-        variables += "(declare-const p0d%dt%d Bool)\n" % (d, t);
+	i = d;
+	for t in range(0, 2 * C + 1):
+		variables += "(declare-const p0d%dt%d Bool)\n" % (d, t);
 
-    # start at middle discrepancy
-    constraints += "(assert p0d%dt%d)\n" % (d, C);
+	# start at middle discrepancy
+	constraints += "(assert p0d%dt%d)\n" % (d, C);
+	for t in range(0, 2 * C + 1):
+		if t != C:
+			constraints += "(assert (not p0d%dt%d))\n" % (d, t);
 
-    while i <= N:
-        for t in range(0, 2 * C + 1):
-            variables += "(declare-const p%dd%dt%d Bool)\n" % (i, d, t);
+	while i <= N:
+		for t in range(0, 2 * C + 1):
+			variables += "(declare-const p%dd%dt%d Bool)\n" % (i, d, t);
 
-            if t > 0:
-                constraints += "(assert (or (not (and p%dd%dt%d xp%d)) p%dd%dt%d))\n" % (i - d, d, t - 1, i, i, d, t);
-            if t < 2 * C:
-                constraints += "(assert (or (not (and p%dd%dt%d xm%d)) p%dd%dt%d))\n" % (i - d, d, t + 1, i, i, d, t);
+			if t > 0:
+				constraints += "(assert (or (not (and p%dd%dt%d xp%d)) p%dd%dt%d))\n" % (i - d, d, t - 1, i, i, d, t);
+			if t < 2 * C:
+				constraints += "(assert (or (not (and p%dd%dt%d xm%d)) p%dd%dt%d))\n" % (i - d, d, t + 1, i, i, d, t);
 
-            constraints += "(assert (not p%dd%dt%d))\n" % (i, d, 0);
-            constraints += "(assert (not p%dd%dt%d))\n" % (i, d, 2 * C);
-        i += d;
+			constraints += "(assert (not p%dd%dt%d))\n" % (i, d, 0);
+			constraints += "(assert (not p%dd%dt%d))\n" % (i, d, 2 * C);
+		i += d;
 
 solve_file = open("data/disc-C%d-n%d.z3" % (C,N), "w");
 solve_file.write(variables + constraints + "(check-sat)\n(get-model)\n");
@@ -53,8 +56,8 @@ check = z3.check();
 print "check: ", check;
 
 if check == sat:
-    print z3.model();
+	print z3.model();
 else:
-    solve_file = open("data/disc-C%d-n%d.z3" % (C,N), "w");
-    solve_file.write("(set-option :produce-proofs true)\n" + variables + constraints + "(check-sat)\n(get-proof)\n");
-    solve_file.close();
+	solve_file = open("data/disc-C%d-n%d.z3" % (C,N), "w");
+	solve_file.write("(set-option :produce-proofs true)\n" + variables + constraints + "(check-sat)\n(get-proof)\n");
+	solve_file.close();
